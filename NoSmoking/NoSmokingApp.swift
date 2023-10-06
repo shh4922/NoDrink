22bd1f2986c1923846dd032b45cb13b3b534d0d1
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2023-10-06 23:40:32 +0900

    Initial Commit

---
+++ NoSmoking/NoSmokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSmokingApp.swift
+//  NoSmoking
+//
+//  Created by Hyeonho on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct NoSmokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
